0f0014a6056ebdd7c6787d43b2f7f87daebcb339
Alberto Penas Amor <alberto.penas@nomasystems.com>
2021-01-26 21:11:50 +0100

    draw pieces when tapping the board

--- app/src/main/java/com/github/albertopeam/gomoku/ui/view/GridView.kt
+++ app/src/main/java/com/github/albertopeam/gomoku/ui/view/GridView.kt
@@ -6,14 +6,17 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import com.github.albertopeam.gomoku.R
 import com.github.albertopeam.gomoku.domain.Board
 import com.github.albertopeam.gomoku.domain.Player
 import com.github.albertopeam.gomoku.domain.Position
 import kotlin.math.min
+import kotlin.math.roundToInt
 
 //TODO: migrate to non squarish board
+//TODO: fix board lines start and end(small breaks)
 class GridView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
 
     private var lineColor: Int = Color.BLACK
@@ -58,6 +61,20 @@ class GridView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
             style = Paint.Style.FILL
             color = Color.BLACK
         }
+        setOnTouchListener { view, motionEvent ->
+            board?.let { board ->
+                if (motionEvent.actionMasked == MotionEvent.ACTION_UP) {
+                    val posX = motionEvent.x
+                    val posY = motionEvent.y
+                    val column = (posX / cellSize()).roundToInt() - 1
+                    val row = (posY / cellSize()).roundToInt() - 1
+                    board.place(Position(row, column), Player.WHITE)
+                    invalidate()
+                }
+            }
+            view.performClick()
+            return@setOnTouchListener true
+        }
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -91,8 +108,7 @@ class GridView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
         canvas?.apply {
             val width = measuredWidth
             val height = measuredHeight
-            val totalLinesWidth = lineWidth * cells
-            val distance = (height - totalLinesWidth) / (cells + 2)
+            val distance = cellSize()
             (1..cells + 1).forEach {
                 val position = (it * distance) + (it * lineWidth)
                 drawLine(distance, position, width.toFloat() - distance, position, linePaint)
@@ -105,14 +121,17 @@ class GridView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
         val xPos = col + 1
         val yPos = row + 1
         canvas?.apply {
-            val height = measuredHeight
-            val totalLinesWidth = lineWidth * cells
-            val distance = (height - totalLinesWidth) / (cells + 2)
+            val distance = cellSize()
             val radiusPadding = lineWidth
-            val radius = (distance - (radiusPadding)) / 2f
-            val xCenter = (distance * xPos) + (lineWidth * xPos) + radius + radiusPadding
-            val yCenter = (distance * yPos) + (lineWidth * yPos) + radius + radiusPadding
+            val radius = (distance - (radiusPadding)) / 2.5f
+            val xCenter = (distance * xPos) + (lineWidth * xPos)// + radius + radiusPadding
+            val yCenter = (distance * yPos) + (lineWidth * yPos)// + radius + radiusPadding
             drawCircle(xCenter, yCenter , radius, paint)
         }
+    }
+
+    private fun cellSize(): Float {
+        val totalLinesWidth = lineWidth * cells
+        return (height - totalLinesWidth) / (cells + 1)
     }
 }
